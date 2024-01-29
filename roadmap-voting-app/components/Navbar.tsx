f6635bbb46d12a45ea0237dac5428c34049ade3e
Adosh Singh <adoshbhanoo@gmail.com>
2024-01-29 17:44:31 +0530

    #00 AS: initializes app with authentication frontend

---
+++ roadmap-voting-app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = () => {
+
+
+
+    return (
+        <div className="bg-cusSec border-cusBorder border w-[50%] text-white flex items-center justify-between mt-5 p-2 rounded-lg">
+            <div className="flex items-center">
+                <Link href="/" className="outline-none">
+                    <Image
+                        src="/assets/logos/devzero_logo.png"
+                        alt="Devzero"
+                        width={75}
+                        height={75}
+                    />
+                </Link>
+                <div>
+                    <h1 className=" text-3xl tracking-wide font-bold">DevZero</h1>
+                    <p className=" text-sm text-gray-400">Roadmap voting app</p>
+                </div>
+            </div>
+            <div className="flex gap-4 items-center px-4">
+                <Link href="/auth/signin" className="outline-none">
+                    <button className="bg-custom-gradient px-4 py-2 rounded-full w-32">
+                        Sign in
+                    </button>
+                </Link>
+                <Link href="/auth/signup" className="outline-none">
+                    <button className="bg-custom-gradient px-4 py-2 rounded-full w-32">
+                        Sign up
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ roadmap-voting-app/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-gradient': 'linear-gradient(to right, #e73c9f, #a032c6, #4125f8)',
+      },
+      colors: {
+        cusBg: "#0f0b29",
+        cusSec: "#120d30",
+        cusInput: "#261046",
+        cusBorder: "#5726A0",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
