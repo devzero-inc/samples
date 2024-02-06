edb67f19c65cb5e4d101060fef200081ef1b9925
Adosh Singh <adoshbhanoo@gmail.com>
2024-02-06 10:47:34 +0530

    #00 AS: initialized the project

---
+++ todo-list-nuxt-app/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import type { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
+  buildModules: ['@nuxt/typescript-build']
+}
+
+export default config
+
+// export default defineNuxtConfig({
+//   devtools: { enabled: true }
+// })
